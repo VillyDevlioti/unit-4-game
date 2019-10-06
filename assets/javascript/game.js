db281c8b0e827a4f4f3b73aca18c4286df73c6ce
Villy Devlioti <vadevlio@gmail.com>
2019-10-05 23:24:28 -0400

    Improvements in code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,62 +8,17 @@ var crystalPoints=[]; // this is where we store all the crystal points
 $(document).ready(function() {
 
     function crystalGameLogic() {
-        $('#crystal-1').on("click",function(){
-            userScore+=crystalPoints[0];
-            console.log("User score updated:",userScore);
+        //I had to look that up, because I had spaghetti code
+        $("div[id*='button-']").on("click", function(){
+            userScore += Number($(this).attr('value'));
             $('#user-score').text(userScore);
-            if(userScore===computerNumber){
-                $('#user-score').text(userScore);
-                wins++;
-                console.log('won');
-                initializeGame();
-            }
-            else if(userScore>computerNumber)
-            {   $('#user-score').text(userScore);
-                losses++;
-                console.log('lost');
-                initializeGame();
-            }
-        });
-        $('#crystal-2').on("click",function(){
-            userScore+=crystalPoints[1];
-            console.log("User score updated:",userScore);
-            $('#user-score').text(userScore);
-            if(userScore===computerNumber){
-                wins++;
-                initializeGame();
-            }
-            else if(userScore>computerNumber)
-            {
+            console.log(userScore);
+            if(userScore>computerNumber){
                 losses++;
                 initializeGame();
             }
-        });
-        $('#crystal-3').on("click",function(){
-            userScore+=crystalPoints[2];
-            console.log("User score updated:",userScore);
-            $('#user-score').text(userScore);
-            if(userScore===computerNumber){
-                wins++;
-                initializeGame();
-            }
-            else if(userScore>computerNumber)
-            {
-                losses++;
-                initializeGame();
-            }
-        });
-        $('#crystal-4').on("click",function(){
-            userScore+=crystalPoints[3];
-            console.log("User score updated:",userScore);
-            $('#user-score').text(userScore);
-            if(userScore===computerNumber){
-                wins++;
-                initializeGame();
-            }
-            else if(userScore>computerNumber)
-            {
-                losses++;
+            else if(userScore===computerNumber) {
+                win++;
                 initializeGame();
             }
         });
@@ -73,6 +28,7 @@ $(document).ready(function() {
 
         //First, assign a new computer score and turn user Score to 0
         userScore=0;
+        console.log(userScore);
         $('#user-score').text(userScore);
         computerNumber = Math.ceil(20+Math.random()*30);
         $('#computer-score').text(computerNumber);
@@ -80,23 +36,27 @@ $(document).ready(function() {
         //Then, assign numbers to crystals
         //First we generate four new numbers and we add them to an array
         for (var i=0; i<4; i++) {
-            newValue = Math.ceil(Math.random()*11);
-            crystalPoints[i]=newValue;
-            console.log("the starting points of each crystal",crystalPoints[i]);
+            //create a new table every time. 
+            //Splice: At position i, remove 1 item, and add a random number
+            crystalPoints.splice(i,1,Math.round(Math.random()*11));
         }
+        console.log(crystalPoints);
+
+        //Then I create a new attribute to assign it to each crystal, because I want to use this and understand how it works!!!
+        $('#button-1').attr('value', crystalPoints[0]);
+        console.log()
+        $('#button-2').attr('value', crystalPoints[1]);
+        $('#button-3').attr('value', crystalPoints[2]);
+        $('#button-4').attr('value', crystalPoints[3]);
     
         //Then write how many wins and losses the user currently has
         $('#wins').text(wins);
         $('#losses').text(losses);
 
-        //Then write the user score
-
         //Now it's time to call the main function
         crystalGameLogic();
     }
 
-    //$('#start-new-game').on("click", function(){
         initializeGame();
-    //});
 
 });
