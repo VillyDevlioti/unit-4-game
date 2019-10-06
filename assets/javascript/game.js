5b677407bf80fa4f061e7da244a077d16574c806
Villy Devlioti <vadevlio@gmail.com>
2019-10-05 21:15:34 -0400

    that's a very serial implementation, just to make sure that things are working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,8 +3,38 @@ var newValue;
 var wins=0;
 var losses=0;
 var userScore=0;
+var crystalPoints=[]; // this is where we store all the crystal points
 
 $(document).ready(function() {
+
+    function crystalGameLogic() {
+            $('#crystal-1').on("click",function(){
+                userScore+=crystalPoints[0];
+                $('#user-score').text(userScore);
+                console.log(userScore);
+                return;
+            });
+            $('#crystal-2').on("click",function(){
+                userScore+=crystalPoints[1];
+                console.log(userScore);
+                $('#user-score').text(userScore);
+                return;
+            });
+            $('#crystal-3').on("click",function(){
+                userScore+=crystalPoints[2];
+                console.log(userScore);
+                $('#user-score').text(userScore);
+                return;
+            });
+            $('#crystal-4').on("click",function(){
+                userScore+=crystalPoints[3];
+                console.log(userScore);
+                $('#user-score').text(userScore);
+                return;
+            });
+
+    }
+
     function initializeGame (){
 
         //First, assign a new computer score
@@ -12,20 +42,24 @@ $(document).ready(function() {
         $('#computer-score').text(computerNumber);
         
         //Then, assign numbers to crystals
-        for (var i=1; i<=4; i++){
-            var newValue = Math.ceil(Math.random()*9);
-            $('#crystal').attr('value', newValue);
-            console.log("value of crystal",i,"is",newValue); // checking that everything works
-        }
+        //First we generate four new numbers and we push them to an array
+        for (var i=0; i<4; i++) {
+            newValue = Math.ceil(Math.random()*9);
+            crystalPoints.push(newValue);
+            console.log(crystalPoints[i]);
 
+        }
+    
         //Then write how many wins and losses the user currently has
         $('#wins').text(wins);
         $('#losses').text(losses);
 
         //Then write the user score
-        $('#user-score').text(userScore);
-    }
+        $('#user-score').text(0);
 
+        //Now it's time to call the main function
+        crystalGameLogic();
+    }
 
     $('#start-new-game').on("click", function(){
         initializeGame();
